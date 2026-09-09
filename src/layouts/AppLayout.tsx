f13166e6a86ca7d63efd0aba03fc22d637481872
seungminng <ssmmkim1025@naver.com>
2026-09-09 20:49:59 +0900

    🐛 fix: AppLayout 세로 여백 수정으로 불필요한 스크롤 제거

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 export default function AppLayout() {
   return (
-    <div className="mx-auto min-h-screen w-full max-w-97.5 px-5 py-5 bg-[#FFF9EF]">
+    <div className="mx-auto min-h-screen w-full max-w-97.5 px-5 pt-5 bg-[#FFF9EF]">
       <Outlet />
     </div>
   );
