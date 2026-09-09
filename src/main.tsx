f6e54ba76dd005690c6e9856cd4860a4d82830d3
seungminng <ssmmkim1025@naver.com>
2026-09-09 17:10:08 +0900

    🔧 fix: Router 중복 설정 수정

--- src/main.tsx
+++ src/main.tsx
@@ -2,21 +2,21 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 
-import App from "./App";
-import "./index.css";
+import App from "@/App";
+import "@/index.css";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    {/* TanStack Query 설정, 서버에서 받아오는 데이터관리 라이브러리*/}
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-
-      <ReactQueryDevtools initialIsOpen={false} />
+      <App />
+      {/* React Query 개발 도구, 캐시된 데이터, API 호출 정보 확인가능 
+        나중에 연동할때 쓰면 좋을듯욤
+        <ReactQueryDevtools initialIsOpen={false} />
+      */}
     </QueryClientProvider>
   </StrictMode>,
 );
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+
+  server: {
+    open: true,
+  },
 });
