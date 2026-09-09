1d0bce5b7b5f0d11cb4a4c378da68eba8dca8466
seungminng <ssmmkim1025@naver.com>
2026-09-09 17:02:03 +0900

    🎨 style: 전역 레이아웃 웹앱 크기 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import AppLayout from "@/layouts/AppLayout";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<div>Readle</div>} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<AppLayout />}>
+          <Route path="/" element={<div>Readle</div>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
