f27e6245a9f2bb2eb92a5910145a5d8f0d305261
seungminng <ssmmkim1025@naver.com>
2026-09-10 19:54:09 +0900

    ✨ feat: 스플래시 화면 및 로그인 라우팅 구현

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AppLayout from "@/layouts/AppLayout";
 import FooterLayout from "@/layouts/FooterLayout";
 
+import SplashPage from "./pages/auth/SplashPage";
+
 import GamePage from "@/pages/game/GamePage";
 import HomePage from "@/pages/home/HomePage";
 import MyPage from "@/pages/my/MyPage";
@@ -22,13 +24,15 @@ export default function App() {
         <Route element={<AppLayout />}>
           {/* 푸터 필요한 페이지 */}
           <Route element={<FooterLayout />}>
-            <Route path="/" element={<HomePage />} />
+            <Route path="/home" element={<HomePage />} />
             <Route path="/ranking" element={<RankingPage />} />
             <Route path="/review" element={<ReviewPage />} />
             <Route path="/game" element={<GamePage />} />
             <Route path="/my" element={<MyPage />} />
           </Route>
           {/* 푸터 필요 없는 페이지는 나중에 여기아래에 */}
+          <Route path="/" element={<SplashPage />} />
+
           <Route path="/game/solo" element={<SoloIntroPage />} />
           <Route path="/game/solo/reading" element={<SoloReadingPage />} />
           <Route path="/game/solo/play" element={<SoloGamePage />} />
---
+++ src/pages/auth/SplashPage.tsx
@@ -0,0 +1,40 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+import splashLogo from "@/assets/icons/splashLogo.png";
+import splashBeluga from "@/assets/images/splashBeluga.png";
+import splashText from "@/assets/icons/splashText.png";
+
+export default function SplashPage() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigate("/login");
+    }, 1500);
+
+    return () => clearTimeout(timer);
+  }, [navigate]);
+
+  return (
+    <main className="relative min-h-screen overflow-hidden bg-[#2285E3]">
+      {/* 로고 + 문구 */}
+      <section className="absolute top-[25%] left-1/2 z-10 flex -translate-x-1/2 flex-col items-center">
+        <img src={splashLogo} alt="READLE" className="w-90 max-w-none object-contain" />
+
+        <img
+          src={splashText}
+          alt="세상에서 가장 재미있는 문해력 훈련, 이거 코드로 해봤는데 효과가 많이 들어간 글씨라서 이미지 넣은게 더 나음"
+          className="mt-5 w-63 object-contain"
+        />
+      </section>
+
+      {/* 벨루가 */}
+      <img
+        src={splashBeluga}
+        alt="귀여운 벨루가 키키"
+        className="absolute bottom-0 left-1/2 w-full max-w-none -translate-x-1/2 object-contain"
+      />
+    </main>
+  );
+}
