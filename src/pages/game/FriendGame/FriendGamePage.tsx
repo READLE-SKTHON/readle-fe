88fbf7f0fa2fad543d74ef089e3a4db85e7ad7a5
seungminng <ssmmkim1025@naver.com>
2026-09-10 11:05:58 +0900

    ✨ feat: 게임 모드 페이지 생성 및 라우팅 추가

---
+++ src/pages/game/FriendGame/FriendGamePage.tsx
@@ -0,0 +1,11 @@
+import Header from "@/components/common/header/Header";
+
+export default function FriendGamePage() {
+  return (
+    <main>
+      <Header title="친구와 함께" />
+
+      <div>친구와 함께 페이지</div>
+    </main>
+  );
+}
